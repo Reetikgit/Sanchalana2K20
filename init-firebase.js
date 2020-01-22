44f9b48aac16c9fd130bf9e248afffe3512103f2
Reetik <reetikchitragupt@gmail.com>
2020-01-22 12:14:55 +0530

    new commit

---
+++ init-firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB_5vqcnQ7sD2AsHeBRl2QB6GifZmp8jGM",
+    authDomain: "contactform-673b7.firebaseapp.com",
+    databaseURL: "https://contactform-673b7.firebaseio.com",
+    projectId: "contactform-673b7",
+    storageBucket: "contactform-673b7.appspot.com",
+    messagingSenderId: "172917494368",
+    appId: "1:172917494368:web:1cc301177c4f4fd6a46905",
+    measurementId: "G-N5BDJDDC63"
+
+  };
+ 
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
